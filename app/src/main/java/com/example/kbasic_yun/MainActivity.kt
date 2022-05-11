c5924bd2204f9b095b91aa313b22d8f2ba4ebef0
Roy <J.jyun0525@gmail.com>
2022-05-11 14:48:15 +0900

    클릭이벤트

--- app/src/main/java/com/example/kbasic_yun/MainActivity.kt
+++ app/src/main/java/com/example/kbasic_yun/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.kbasic_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickBtn.setOnClickListener {
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+            Log.e("메인화면로그", "에러 관련 로그")
+        }
+        
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
     }
 }
