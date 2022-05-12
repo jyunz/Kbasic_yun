c5c19f346299258f3d8f27773f07558e77f2e23c
Roy <J.jyun0525@gmail.com>
2022-05-12 15:29:09 +0900

    로그인 토스트

--- app/src/main/java/com/example/kbasic_yun/MainActivity.kt
+++ app/src/main/java/com/example/kbasic_yun/MainActivity.kt
@@ -10,8 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        clickBtn.setOnClickListener {
 
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPassword = pwEdt.text.toString()
+
+            if (inputId == "admin@test.com" && inputPassword == "qwer") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
 
         }
 
